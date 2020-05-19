3f97190276cbc83a9d80aa519bb4ac27dae24882
Golbakh Mikhail <golbah@yandex.ru>
2020-05-19 15:12:03 +0300

    Add serialization for KvsCommand

--- src/main/java/ru/andrey/kvstorage/jclient/command/KvsCommand.java
+++ src/main/java/ru/andrey/kvstorage/jclient/command/KvsCommand.java
@@ -1,20 +1,8 @@
 package ru.andrey.kvstorage.jclient.command;
 
-import java.util.List;
+import ru.andrey.kvstorage.resp.object.RespObject;
 
 public interface KvsCommand {
 
-    List<String> asList();
-
-//    String getName(); // todo think about this we can use it in this#toApiBytesRepresentation
-//    List<String> getArgs();
-
-    default byte[] toApiBytesRepresentation() {
-        return String.join(" ", asList()).getBytes(); // todo sukhoa selialize to array (RESP protocol)
-//        List<String> list = asList();
-//        String startArray = "#" + list.size() + separator;
-//        return list.stream()
-//                .map(t -> "$" + t + separator)
-//                .collect(Collectors.joining()).getBytes();
-    }
+    RespObject serialize();
 }
