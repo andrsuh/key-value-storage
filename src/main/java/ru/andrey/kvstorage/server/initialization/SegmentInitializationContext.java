2db968fe4cd5f12e5a3326d04b0737cbb4c9655b
Golbakh Mikhail <golbah@yandex.ru>
2020-05-27 07:08:47 +0300

    Add documentation for the initialization package

--- src/main/java/ru/andrey/kvstorage/server/initialization/SegmentInitializationContext.java
+++ src/main/java/ru/andrey/kvstorage/server/initialization/SegmentInitializationContext.java
@@ -6,11 +6,32 @@ import ru.andrey.kvstorage.server.index.impl.SegmentIndex;
 import java.nio.file.Path;
 
 public interface SegmentInitializationContext {
+
+    /**
+     * Возвращает имя инициализируемого сегмента.
+     *
+     * @return имя инициализируемого сегмента
+     */
     String getSegmentName();
 
+    /**
+     * Возвращает путь до файла сегмента.
+     *
+     * @return путь до файла сегмента
+     */
     Path getSegmentPath();
 
+    /**
+     * Возвращает индекс инициализируемого сегмента.
+     *
+     * @return индекс инициализируемого сегмента
+     */
     SegmentIndex getIndex();
 
+    /**
+     * Возвращает текущий размер инициализируемого сегмента.
+     *
+     * @return текущий размер инициализируемого сегмента
+     */
     int getCurrentSize(); // todo sukhoa should be long
 }
