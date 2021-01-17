936a81b5df6cd46a4852448c18bb2fcbcc0cc177
asukhovitsky <andrsuh13@yandex.ru>
2021-01-17 22:50:10 +0300

    remove magic constant

--- src/main/java/ru/andrey/kvstorage/jclient/connection/SocketConnection.java
+++ src/main/java/ru/andrey/kvstorage/jclient/connection/SocketConnection.java
@@ -16,9 +16,9 @@ public class SocketConnection implements KvsConnection {
     private final RespReader reader;
     private final RespWriter writer;
 
-    public SocketConnection() {
+    public SocketConnection(ConnectionConfig config) {
         try {
-            this.socket = new Socket("127.0.0.1", 4321);// todo sukhoa remove magic constant
+            this.socket = new Socket(config.getHost(), config.getPort());
             this.reader = new RespReader(new BufferedInputStream(this.socket.getInputStream()));
             this.writer = new RespWriter(new BufferedOutputStream(this.socket.getOutputStream()));
         } catch (IOException e) {
